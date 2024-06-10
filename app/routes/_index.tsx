b250605ad20773e62ae9f385055fc2e2c424c046
Hayatann <r121317@g.kobe-kosen.ac.jp>
2024-06-10 22:22:18 +0900

    first commit

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,4 +1,16 @@
 import type { MetaFunction } from "@remix-run/cloudflare";
+import { json } from "@remix-run/cloudflare";
+import { useLoaderData } from "@remix-run/react";
+
+type Expense = {
+  year: number;
+  month: number;
+  day: number;
+  use: string;
+  price: number;
+  who: string;
+  memo: string;
+};
 
 export const meta: MetaFunction = () => {
   return [
@@ -10,7 +22,17 @@ export const meta: MetaFunction = () => {
   ];
 };
 
+export const loader = async () => {
+  const res = await fetch(
+    "https://script.google.com/macros/s/AKfycbxI7AxP_WciY4DKOR8BGc8FmHnkFajujx7EWG4SNq0fTFn-TJMoO9LSv0foKRmFeIOA/exec?action=getAllData"
+  );
+  const data = await res.json();
+  return json({ data });
+};
+
 export default function Index() {
+  const expenses = useLoaderData<typeof loader>().data as Expense[];
+
   return (
     <div style={{ fontFamily: "system-ui, sans-serif", lineHeight: "1.8" }}>
       <h1>Welcome to Remix on Cloudflare Workers</h1>
@@ -30,6 +52,16 @@ export default function Index() {
           </a>
         </li>
       </ul>
+      <div>
+        {expenses.map((expense: Expense, index: number) => (
+          <div key={index}>
+            <p>
+              {expense.year}/{expense.month}/{expense.day} {expense.use}{" "}
+              {expense.price} {expense.who} {expense.memo}
+            </p>
+          </div>
+        ))}
+      </div>
     </div>
   );
 }
